f08fe891469e2ac62f8b91b77fff4743aca9f73e
LilyLiu <lienlien910130@gmail.com>
2022-08-04 16:45:03 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,48 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js Appaa" />
+  <HelloWorld :msg="msg" :profile="profile" />
 </template>
 
 <script>
+import { onBeforeMount, reactive, ref } from "@vue/runtime-core";
+export default {
+  setup() {
+    var msg = ref("");
+    var profile = reactive({});
+    onBeforeMount(() => {
+      window.liff
+        .init({
+          liffId: "1657358109-M7NJYBln",
+        })
+        .then(() => {
+          getProfile();
+        });
+    });
+    function getProfile() {
+      window.liff.getProfile().then((response) => {
+        profile = response;
+        msg.value = profile.displayName;
+      });
+    }
+    return { msg: msg, profile: profile };
+  },
+};
+</script>
+
+<!-- <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import { ref, reactive, onMounted, onBeforeMount, inject } from "vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
   },
+  onBeforeMount(() => {
+    
+  }),
 };
-</script>
+</script> -->
 
 <style>
 #app {
