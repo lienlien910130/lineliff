d850ec525a261621501728cd9a3ea05d998d1f07
lily <lienlien910130@gmail.com>
2022-08-05 20:24:19 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,33 @@
-import { createRouter,  createWebHistory } from "vue-router";
+import { createRouter,  createWebHashHistory,  createWebHistory } from "vue-router";
 
 const routes = [
   {
     path: "/",
     name: "index",
-    component: () => import("@/index.vue"),
-    meta: {
-        layout:null,
-      title: "訂婚宴管理",
-    },
-  },
-  {
-    path: "/register",
-    name: "register",
     component: () => import("@/register.vue"),
     meta: {
-        layout:null,
       title: "馨蓮&宗桓婚禮出席表單",
+    },
+    
+  },
+  {
+    path: "/back",
+    name: "back",
+    component: () => import("@/index.vue"),
+    meta: {
+      title: "訂婚宴管理",
     },
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 });
 
 export function resetRouter() {
   const newRouter = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
   });
 
